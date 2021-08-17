a75c96df7e4bcde611c709f514d2ad6601cc4b9b
Xing Voong <xingvoong@gmail.com>
2021-08-17 02:33:33 -0700

    checkpoint

--- server/model/questions.js
+++ server/model/questions.js
@@ -7,23 +7,6 @@ const getQuestions = (product_id) => {
   return pool.query(query, value);
 };
 
-
-// select * from
-//   (select * from questions where product_id = '1000011') as t1
-// left outer join
-//   (select answers.question_id, array_agg(answers.id) as answers_id from answers where question_id = '3518962' group by question_id ) as t2
-// on t1.id = t2.question_id
-
-select * from
-  (select * from questions where product_id = '1000011') as t1
-left outer join
-  (select answers.question_id, array_agg(answers.id) as answers_id from answers where question_id = '3518962' group by question_id ) as t2
-on t1.id = t2.question_id
-
-
-
-
-
 const addQuestion = (product_id, body, date_written, username) => {
   const value = [parseInt(product_id, 10), body, parseInt(Date.now(), 10), username];
   const query = 'select users.id from users where username = $1';
@@ -39,3 +22,69 @@ const addQuestion = (product_id, body, date_written, username) => {
 module.exports = {
   getQuestions, addQuestion,
 };
+
+// join with question_id
+select p.* from
+  (select * from answers
+  inner join
+    (
+      select
+        answer_id, json_agg(url) as photos
+      from photos where photos.answer_id = '5'
+      group by answer_id
+    ) as p2
+  on answers.id = p2.answer_id) as p
+
+select product_id,
+  (json_agg(row_to_json((t1.id, body, date_written, asker_id, helpful, reported, answers_data)))) as questions_data
+from
+  (select * from questions where product_id = '1000011') as t1
+left outer join
+  (select p.question_id,
+    json_agg(row_to_json((p.id, p.body, p.date_written, p.answerer_id, p.helpful, p.reported, p.photos))) as answers_data
+  from
+    (
+      select p1.*
+      from
+        (select * from answers
+        inner join
+          (
+            select
+              answer_id, json_agg(url) as photos
+            from photos
+            group by photos.answer_id
+          ) as p2
+        on answers.id = p2.answer_id) as p1
+    ) as p
+  group by question_id) as t2
+on t1.id = t2.question_id
+group by product_id
+
+//question_id = 1
+//answer_id = 5
+
+
+select product_id,
+  (json_agg(row_to_json((t1.id, body, date_written, asker_id, helpful, reported, answers_data)))) as questions_data
+from
+  (select * from questions where product_id = '1000011') as t1
+left outer join
+  (select p.question_id,
+    json_agg(row_to_json((p.id, p.body, p.date_written, p.answerer_id, p.helpful, p.reported, p.photos))) as answers_data
+  from
+    (
+      select p1.*
+      from
+        (select * from answers
+        inner join
+          (
+            select
+              answer_id, json_agg(url) as photos
+            from photos
+            group by photos.answer_id
+          ) as p2
+        on answers.id = p2.answer_id) as p1
+    ) as p
+  group by question_id) as t2
+on t1.id = t2.question_id
+group by product_id
