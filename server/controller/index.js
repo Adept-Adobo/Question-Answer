349605e5218631c34d752cef8834345a9833b476
Xing Voong <xingvoong@gmail.com>
2021-08-20 17:14:25 -0700

    checkpoint

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,1 +1,2 @@
 exports.questions = require('./questions');
+exports.hello = require('./hello');
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.get('/');
+router.get('/', (req, res) => {
+  res.send('hello');
+});
+
+router.get('/loaderio-961dc0d3598dba0f66ca49447c374b90.txt', (req, res) => {
+  res.send('loaderio-961dc0d3598dba0f66ca49447c374b90');
+});
 
 router.get('/questions/:product_id', controller.questions.get);
 
